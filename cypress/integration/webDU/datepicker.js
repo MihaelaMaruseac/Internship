58295605c3f0a3814a078444e81ecba40c98c032
Mihaela Maruseac <mihaelapopescu2011@gmail.com>
2021-10-13 15:30:57 +0300

    Date picker

---
+++ cypress/integration/webDU/datepicker.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+
+describe('Test datepicker via WebDU', () => {
+
+    it('Select date from the datepicker', () =>{
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#datepicker').invoke('removeAttr', 'target').click({force:true})
+        cy.get('#datepicker').click()
+
+        // let date = new Date();
+        // date.setDate(date.getDate()) // get the current day
+        // cy.log(date.getDate())
+
+        // let date2 = new Date();
+        // date2.setDate(date.getDate() + 5)
+        // cy.log(date2.getDate())
+
+        var date = new Date();
+        date.setDate(date.getDate() +1)
+
+        var futureYear = date.getFullYear();
+        var futureMonth = date.toLocaleString("default", {month: "long"})
+        var futureDay = date.getDate();
+
+        cy.log("Future year to select: "+ futureYear)
+        cy.log("Future month to select: "+ futureMonth)
+        cy.log("Future day to select: "+ futureDay)
+
+        function selectMonthAndYear(){
+            cy.get('.datepicker-dropdown').find('.datepicker-switch').first().then(currentDate => {
+                if(!currentDate.text().includes(futureYear)){
+                    cy.get('.next').first().click();
+                    selectMonthAndYear();
+                }
+            }).then(() =>{
+                cy.get('.datepicker-dropdown').find('.datepicker-switch').first().then(currentDate => {
+                    if(!currentDate.text().includes(futureMonth)){
+                        cy.get('.next').first().click();
+                        selectMonthAndYear();
+                    }
+                })
+            })
+        }
+
+        function selectFutureDay(){
+            cy.get('[class="day"]').contains(futureDay).click();
+        }
+
+        selectMonthAndYear();
+        selectFutureDay();
+    })
+})
